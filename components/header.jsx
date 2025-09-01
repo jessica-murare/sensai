c9b4eefe5e0b9777790183a2cdc23977165c31e0
jessica-murare <yadavkhushi.31.01@gmail.com>
2025-09-02 01:00:18 +0530

    initial commit

---
+++ components/header.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { SignInButton } from "@clerk/nextjs";
+import { SignedOut } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
+import { SignedIn } from "@clerk/nextjs";
+
+const Header = () => {
+    return (
+        <div>
+            <SignedOut>
+                <SignInButton />
+            </SignedOut>
+            <SignedIn>
+                <UserButton />
+            </SignedIn>
+        </div>
+    );
+};
+
+export default Header;
